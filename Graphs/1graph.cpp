ca8e0640599e130d951ea7fa3c2b7c1c0f79568b
satakratu6 <satakratu.madhu@gmail.com>
2025-02-12 23:25:09 +0530

    newww

--- Graphs/1graph.cpp
+++ Graphs/1graph.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 class Graph
 {
 public:
@@ -11,23 +12,24 @@ public:
     vertices = v;
     edges.resize(v);
   }
+
   void addEdge(int u, int v)
   {
     edges[u].push_back(v);
     edges[v].push_back(u);
   }
+
   void BFS(int start)
   {
     vector<bool> visited(vertices, 0);
     queue<int> q;
     q.push(start);
-    visited[start]=true;
+    visited[start] = true;
     while (!q.empty())
     {
       int curr = q.front();
       q.pop();
       cout << curr << " ";
-      // iterate over edges[curr]
       for (auto x : edges[curr])
       {
         if (!visited[x])
@@ -37,18 +39,71 @@ public:
         }
       }
     }
+    cout << endl;
   }
-  void DFS_recursion(int start, vector<int>& visited){
-    //node data print
-    visited[start]=true;
-    cout<<start<<" ";
-    for(int x:edges[start]){
-      if(!visited[start])
-      DFS_recursion(x,visited);
+
+  void DFS_recursion(int start, vector<int> &visited)
+  {
+    visited[start] = true;
+    cout << start << " ";
+    for (int x : edges[start])
+    {
+      if (!visited[x])
+        DFS_recursion(x, visited);
     }
   }
-  void DFS(int start){
-    vector<int> visited;
-    DFS_recursion(start,visited);
+
+  void DFS(int start)
+  {
+    vector<int> visited(vertices, 0);
+    DFS_recursion(start, visited);
+    cout << endl;
+  }
+
+  void displayAdjList()
+  {
+    for (int i = 0; i < edges.size(); i++)
+    {
+      cout << i << ": ";
+      for (int j : edges[i])
+      {
+        cout << j << " ";
+      }
+      cout << endl;
+    }
   }
 };
+
+int main()
+{
+  int vertices, edges;
+  cout << "Enter the number of vertices: ";
+  cin >> vertices;
+  Graph g(vertices);
+
+  cout << "Enter the number of edges: ";
+  cin >> edges;
+
+  cout << "Enter the edges (u v):" << endl;
+  for (int i = 0; i < edges; i++)
+  {
+    int u, v;
+    cin >> u >> v;
+    g.addEdge(u, v);
+  }
+
+  cout << "Adjacency List:" << endl;
+  g.displayAdjList();
+
+  int start;
+  cout << "Enter the starting vertex for BFS and DFS: ";
+  cin >> start;
+
+  cout << "BFS Traversal: ";
+  g.BFS(start);
+
+  cout << "DFS Traversal: ";
+  g.DFS(start);
+
+  return 0;
+}
